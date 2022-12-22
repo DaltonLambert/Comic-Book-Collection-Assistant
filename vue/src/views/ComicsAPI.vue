e41cf6025c01044d3ae399d49d1d52ff388fac1d
Dalton <lambertd757@gmail.com>
2022-12-21 23:02:47 -0500

    added create collection button, working on making it so you can drag and drop a comic into a collection

--- vue/src/views/ComicsAPI.vue
+++ vue/src/views/ComicsAPI.vue
@@ -1,17 +1,53 @@
 <template>
-  <div>
-    <div v-for="(row, index) in rows" :key="index" class="row">
-      <div v-for="(comic, index) in row" :key="index" class="comicsAPI" @click="comic.flip = !comic.flip" v-bind:flip="comic.flip">
-        <img v-if="!comic.flip" :src="comic.thumbnail.path + '.' + comic.thumbnail.extension" alt="Comic book thumbnail">
-        <div v-else>
-          <h1>{{ comic.title }}</h1>
-          <h2>Issue #{{ comic.issueNumber }}</h2>
-          <p>{{ comic.description }}</p>
+  <div class="container">
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <h2 class="collection-title">Collections</h2>
+      <div class="new-collection-button" @click="createNewCollection">
+        <div class="plus-sign">+</div>
+        <div class="button-label">Create New Collection</div>
+      </div>
+      <div v-if="showPopup" class="popup">
+        <div class="popup-header">Enter A Name For Your Collection</div>
+        <form class="collection-form" @submit.prevent="submitCollectionName">
+          <input type="text" v-model="collectionName" placeholder="Collection Name">
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+     
+
+      <div v-for="(collection, index) in collections" :key="index" class="collection" @dragover.prevent @drop="drop($event, collection)" @click="openPopup(collection)" >
+        <div class="collection-name">{{ collection.name }}</div>
+      </div>
+    </div>
+
+    <!-- Main area -->
+    <div class="main-area">
+      <div v-for="(row, index) in rows" :key="index" class="row">
+        <div v-for="(comic, index) in row" :key="index" class="comicsAPI" @click="comic.flip = !comic.flip" v-bind:flip="comic.flip" draggable @dragstart="dragStart($event, comic)">
+          <img v-if="!comic.flip" :src="comic.thumbnail.path + '.' + comic.thumbnail.extension" alt="Comic book thumbnail">
+          <div v-else>
+            <h1 class="title">{{ comic.title }}</h1>
+            <h2 class="issueNumber">Issue #{{ comic.issueNumber }}</h2>
+            <p class="description">{{ comic.description }}</p>
+          </div>
         </div>
       </div>
     </div>
+
+
+    <div v-if="showCollectionPopup" class="popup">
+  <div class="popup-header">Comics in Collection: {{ selectedCollection.name }}</div>
+  <div class="comic-list">
+    <div v-for="(comic, index) in selectedCollection.comics" :key="index" class="comic">
+      {{ comic.title }} (Issue #{{ comic.issueNumber }})
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
+
 
 
 <script>
@@ -20,6 +56,11 @@ export default {
     return {
       comics: [],
       rows: [],
+      showPopup: false,
+      collectionName: '',
+      collections: [],
+      selectedCollection: null,
+    showCollectionPopup: false,
     };
   },
 
@@ -47,17 +88,33 @@ flip: false,
 }
      // split comics into rows of 6 comics
      while (this.comics.length > 0) {
-      this.rows.push(this.comics.splice(0, 6));
+      this.rows.push(this.comics.splice(0, 5));
     }
   })
   .catch(error => {
     console.error(error);
   });
   },
-  computed: {
-    cleanImageUrl(url) {
-      return url.replace(/.jpg$/i, '/clean.jpg');
+  methods: {
+    createNewCollection() {
+      this.showPopup = true;
     },
+    submitCollectionName() {
+      this.collections.push({ name: this.collectionName, comics: [] });
+      this.showPopup = false;
+      this.collectionName = '';
+    },
+    dragStart(event, comic) {
+    event.dataTransfer.setData('comic', JSON.stringify(comic));
+  },
+  drop(event, collection) {
+    const comic = JSON.parse(event.dataTransfer.getData('comic'));
+    collection.comics.push(comic);
+  },
+  openPopup(collection) {
+    this.selectedCollection = collection;
+    this.showCollectionPopup = true;
+  },
   },
 };
 </script>
@@ -77,6 +134,7 @@ flip: false,
     transition: transform 0.6s;
     transform-style: preserve-3d;
     float: left;
+    overflow: hidden;
   }
 
   .comicsAPI img {
@@ -106,6 +164,144 @@ flip: false,
   .row {
   clear: both;
 }
+
+.title {
+  font-size: 20px;
+  font-family: Arial, sans-serif;
+  margin: 0;
+  text-align: center;
+  overflow-wrap: break-word; /* wrap text if it is too long to fit on one line */
+  word-wrap: break-word; /* same as above */
+}
+
+.issueNumber {
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+  margin: 0;
+  text-align: center;
+  overflow-wrap: break-word; /* wrap text if it is too long to fit on one line */
+  word-wrap: break-word; /* same as above */
+}
+
+.description {
+  font-size: 14px;
+  font-family: Arial, sans-serif;
+  margin: 0;
+  text-align: center;
+  overflow-wrap: break-word; /* wrap text if it is too long to fit on one line */
+  word-wrap: break-word; /* same as above */
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+.sidebar {
+  flex: 0 0 25%; /* This will set the width of the sidebar to 25% of the container */
+  height: 100%; /* This will make the sidebar the same height as the container */
+  order: 2; /* This will move the sidebar to the right side of the main area */
+}
+
+.main-area {
+  flex: 0 0 75%; /* This will set the width of the main area to 75% of the container */
+  height: 100%; /* This will make the main area the same height as the container */
+  order: 1; /* This will move the main area to the left side of the sidebar */
+}
+
+.collection-title {
+  display: inline-block;
+}
+
+.collection-button {
+  display: inline-block;
+  margin-left: 10px;
+}
+
+.new-collection-button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 150px;
+    height: 150px;
+    background-color: white;
+    border: 2px solid black;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .plus-sign {
+    font-size: 70px;
+    line-height: 70px;
+  }
+
+  .button-label {
+    font-size: 18px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  .popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .popup-header {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  .collection-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .collection-form input[type="text"] {
+    width: 200px;
+    height: 30px;
+    font-size: 18px;
+    margin-bottom: 10px;
+    padding: 0 10px;
+  }
+
+  .collection-form button[type="submit"] {
+    width: 100px;
+    height: 30px;
+    font-size: 18px;
+    background-color: white;
+    border: 2px solid black;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .collection {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 150px;
+    height: 150px;
+    background-color: white;
+    border: 2px solid black;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+
+  .collection-name {
+    font-size: 18px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
 </style>
 
 
