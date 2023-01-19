f1e7cff40f7e43eeb113fa6217a1cdb66a9c954f
Dalton <lambertd757@gmail.com>
2023-01-19 12:35:31 -0500

    Removed unused file

--- vue/src/services/ComicService.js
+++ vue/src/services/ComicService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //not sure if we need to make a http = axios.create statement like greg does in his video. there's something in here where the baseURL is built in i think? question for david?
-//timestamp in video is 1h35min ish
+//timestamp in video is 1h35min ish 
 /*
 const http = axios.create({
     baseURL: "http://localhost:9000"
